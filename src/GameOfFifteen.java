4052820449930a599d4cb54d45f67cedc7e45f88
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2020-10-23 22:26:01 +0200

    Add function "isCompleted()" and remove hard code

--- src/GameOfFifteen.java
+++ src/GameOfFifteen.java
@@ -7,14 +7,18 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class GameOfFifteen extends JFrame implements ActionListener {
 
-    private Brick[][] buttons = new Brick [4][4];
+    private Brick[][] buttons;
     private Brick emptyButton;
+    int xGrid = 4;
+    int yGrid = 4;
+    int gridSize = xGrid * yGrid;
 
     GameOfFifteen() {
 
-        setLayout(new GridLayout(4, 4));
-        //createButtons(); activate this, remove the line below and everthing will go back to where we left.
-        shuffleArray(createButtons());
+
+        setLayout(new GridLayout(xGrid, yGrid));
+        createButtons(); //activate this, remove the line below and everthing will go back to where we left.
+        //shuffleArray(createButtons());
         setSize(300, 300);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -25,29 +29,30 @@ public class GameOfFifteen extends JFrame implements ActionListener {
     Brick [][] array was created instead of int [][], the expected return is therefore Brick.
      */
     public Brick[][] createButtons() {
-        for (int x = 0; x < 4; x++)
-            for (int y = 0; y < 4; y++) {
+        buttons = new Brick[xGrid][yGrid];
 
-                buttons[x][y] = new Brick(x,y);
-                buttons[x][y].setText(x * 4 + y + 1 + "");
+        for (int x = 0; x < xGrid; x++)
+            for (int y = 0; y < yGrid; y++) {
+                buttons[x][y] = new Brick(x, y);
+                buttons[x][y].setText( String.valueOf(x * xGrid + y + 1));
                 buttons[x][y].addActionListener(this);
                 add(buttons[x][y]);
             }
 
-        buttons[3][3].setText("");
-        buttons[3][3].setEnabled(false);
-        emptyButton =buttons [3][3];
+        buttons[xGrid - 1][yGrid - 1].setText("");
+        buttons[xGrid - 1][yGrid - 1].setEnabled(false);
+        emptyButton = buttons[xGrid - 1][yGrid - 1];
         return buttons;
     }
-/* I don't understand what's wrong. I've check and this method is right, this is how we shuffle the
-elements of the array. I think the problem is "empty button" that is fixed or something like that, but I don't
-know. I'm sure it will need a really really small fix.
- */
-    public void shuffleArray(Brick[][] buttons)
-    {
+
+    /* I don't understand what's wrong. I've check and this method is right, this is how we shuffle the
+    elements of the array. I think the problem is "empty button" that is fixed or something like that, but I don't
+    know. I'm sure it will need a really really small fix.
+     */
+    public void shuffleArray(Brick[][] buttons) {
         Random rnd = ThreadLocalRandom.current();
         for (int i = 3; i > 0; i--)
-            for (int j = 3; j > 0; j--){
+            for (int j = 3; j > 0; j--) {
 
                 int indexX = rnd.nextInt(i + 1);
                 int indexY = rnd.nextInt(j + 1);
@@ -56,13 +61,13 @@ know. I'm sure it will need a really really small fix.
                 buttons[indexX][indexY] = buttons[i][j];
                 buttons[indexX][indexY] = buttons[i][j];
                 buttons[i][j] = ab;
-        }
+            }
         refresh();
     }
 
-    public void moveButton (Brick selectedButton) {
-        int tempX= selectedButton.getXPos();
-        int tempY= selectedButton.getYPos();
+    public void moveButton(Brick selectedButton) {
+        int tempX = selectedButton.getXPos();
+        int tempY = selectedButton.getYPos();
 
         selectedButton.setXPos(emptyButton.getXPos());
         selectedButton.setYPos(emptyButton.getYPos());
@@ -74,16 +79,39 @@ know. I'm sure it will need a really really small fix.
 
     }
 
-    public void refresh(){
-        for (int x = 0; x < 4; x++)
-            for (int y = 0; y < 4; y++) {
+    // TODO remove harcode size Look create Buttons
+    public void refresh() {
+        for (int x = 0; x < xGrid; x++)
+            for (int y = 0; y < yGrid; y++) {
                 remove(buttons[x][y]);
             }
-        for (int x = 0; x < 4; x++)
-            for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < xGrid; x++)
+            for (int y = 0; y < yGrid; y++) {
                 add(buttons[x][y]);
             }
         revalidate();
+    }
+
+    private boolean isCompleted() {
+        boolean isSuccess = true;
+        if (emptyButton.getXPos() == xGrid - 1 && emptyButton.getYPos() == yGrid - 1) {
+            int count = 0;
+            for (int x = 0; x < xGrid; x++)
+            {
+                for (int y = 0; y < yGrid; y++) {
+                    count++;
+                    int buttonNumber = buttons[x][y].getText() == "" ?  gridSize : Integer.valueOf(buttons[x][y].getText());
+                    if ( buttonNumber != count) {
+                        isSuccess = false;
+                        break;
+                    }
+                }
+            }
+        } else
+            isSuccess = false;
+
+        System.out.println(isSuccess);
+        return isSuccess;
     }
 
     @Override
@@ -91,14 +119,18 @@ know. I'm sure it will need a really really small fix.
         Brick selectedButton = (Brick) ae.getSource();
         if (selectedButton.isNextTo(emptyButton)) {
             moveButton(selectedButton);
+              if (isCompleted())
+                  System.out.println("suuu");
             refresh();
             //TODO: check if the game is completed successfully.
             //TODO: if not completed sucessfully, refresh into a new game (insert button "play again")
         }
 
+
     }
+
     public static void main(String[] args) {
-	GameOfFifteen GameOfFifteenDemo = new GameOfFifteen();
+        GameOfFifteen GameOfFifteenDemo = new GameOfFifteen();
     }
 
 
